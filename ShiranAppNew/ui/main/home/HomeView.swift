10df97ba788b7458935e53ca3a8e9e1fec1dc66f
user <katatumura615@gmail.com>
2022-03-14 00:52:10 +0900

    ホームに人型をつくる、文字列のstrクラスに設定

--- ShiranAppNew/ui/main/home/HomeView.swift
+++ ShiranAppNew/ui/main/home/HomeView.swift
@@ -60,97 +60,98 @@ struct HomeView: View{
             .navigationBarTitleDisplayMode(.inline)
             .toolbar{
                 ToolbarItem(placement: .navigationBarTrailing){
-                    Menu{
-                        Button(action: {
-                            appState.isSettingView = true
-                        }) {
-                            Text(str.set.rawValue)
-                            Image(systemName: "gear")//"gearshape")
-                        }
-                        Button(action: {
-                            appState.isExplainView = true
-                        }) {
-                            Text(str.detailApp.rawValue)
-                            Image(systemName: "info.circle")//"gearshape")
-                        }
-                        /*
-                         if self.appState.isinAccount {
-                         Button(action: {
-                         appState.logout()
-                         }) {
-                         Text("ログアウト")
-                         Image(systemName: "person")
-                         }
-                         }else{
-                         Button(action: {
-                         appState.isLogin = true
-                         }) {
-                         Text("アカウント設定")
-                         Image(systemName: "person")
-                         }
-                         }*/
-                        
-                        Button(action: {
-                            appState.isPrivacyPolicy = true
-                        }) {
-                            Text(str.privacy_policy.rawValue)
-                            Image(systemName: "shield")
-                        }
-                        Button(action: {
-                            guard let writeReviewURL = URL(string: "https://twitter.com/G0DhdLNn4SftTjc")
-                            else { fatalError("Expected a valid URL") }
-                            UIApplication.shared.open(writeReviewURL)
-                        }) {
-                            Text(str.twitter.rawValue)
-                            Image(systemName: "rectangle.and.pencil.and.ellipsis")
-                        }
-                        Button(action: {
-                            appState.isOpinionView = true
-                        }) {
-                            Text(str.opinion.rawValue)
-                            Image(systemName: "rectangle.and.pencil.and.ellipsis")
-                        }
-                        /*
-                         Button(action: {
-                         guard let writeReviewURL = URL(string: "https://apps.apple.com/us/app/%E3%81%97%E3%82%89%E3%82%93%E3%83%97%E3%83%AA/id1584268203")
-                         else { fatalError("Expected a valid URL") }
-                         UIApplication.shared.open(writeReviewURL)
-                         }) {
-                         Text("アプリを評価する")
-                         Image(systemName: "rectangle.and.pencil.and.ellipsis")
-                         }*/
-                        
-                        
-                         Button(action: {
-                         UserDefaults.standard.removeAll()
-                         exit(0)
-                         }) {
-                             Text(str.reset.rawValue)
-                         Image(systemName: "shield")
-                         }
-                         
-                         Button(action: {
-                         let LastTimeDay = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
-                         UserDefaults.standard.set(LastTimeDay, forKey: Keys._LastTimeDay.rawValue)
-                         }) {
-                             Text(str.oneDayReset.rawValue)
-                         Image(systemName: "shield")
-                         }
-                         
-                        /*
-                         @IBAction func share() {
-                         //share(上の段)から遷移した際にシェアするアイテム
-                         let activityItems: [Any] = [shareText, shareUrl]
-                         let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: [LINEActivity(message: shareText), TwitterActivity(message: shareText)])
-                         self.present(activityViewController, animated: true, completion: nil)
-                         }
-                         */
-                    }label: {
-                        Image(systemName: "line.horizontal.3")
-                            .resizable()
-                            .frame(width: 25, height: 15, alignment: .topLeading)
-                    }
-                    .padding(EdgeInsets(top: 20, leading: 0, bottom: 8, trailing: 10))
+                    toolbarItemView()
+//                    Menu{
+//                        Button(action: {
+//                            appState.isSettingView = true
+//                        }) {
+//                            Text(str.set.rawValue)
+//                            Image(systemName: "gear")//"gearshape")
+//                        }
+//                        Button(action: {
+//                            appState.isExplainView = true
+//                        }) {
+//                            Text(str.detailApp.rawValue)
+//                            Image(systemName: "info.circle")//"gearshape")
+//                        }
+//                        /*
+//                         if self.appState.isinAccount {
+//                         Button(action: {
+//                         appState.logout()
+//                         }) {
+//                         Text("ログアウト")
+//                         Image(systemName: "person")
+//                         }
+//                         }else{
+//                         Button(action: {
+//                         appState.isLogin = true
+//                         }) {
+//                         Text("アカウント設定")
+//                         Image(systemName: "person")
+//                         }
+//                         }*/
+//
+//                        Button(action: {
+//                            appState.isPrivacyPolicy = true
+//                        }) {
+//                            Text(str.privacy_policy.rawValue)
+//                            Image(systemName: "shield")
+//                        }
+//                        Button(action: {
+//                            guard let writeReviewURL = URL(string: "https://twitter.com/G0DhdLNn4SftTjc")
+//                            else { fatalError("Expected a valid URL") }
+//                            UIApplication.shared.open(writeReviewURL)
+//                        }) {
+//                            Text(str.twitter.rawValue)
+//                            Image(systemName: "rectangle.and.pencil.and.ellipsis")
+//                        }
+//                        Button(action: {
+//                            appState.isOpinionView = true
+//                        }) {
+//                            Text(str.opinion.rawValue)
+//                            Image(systemName: "rectangle.and.pencil.and.ellipsis")
+//                        }
+//                        /*
+//                         Button(action: {
+//                         guard let writeReviewURL = URL(string: "https://apps.apple.com/us/app/%E3%81%97%E3%82%89%E3%82%93%E3%83%97%E3%83%AA/id1584268203")
+//                         else { fatalError("Expected a valid URL") }
+//                         UIApplication.shared.open(writeReviewURL)
+//                         }) {
+//                         Text("アプリを評価する")
+//                         Image(systemName: "rectangle.and.pencil.and.ellipsis")
+//                         }*/
+//
+//
+//                         Button(action: {
+//                         UserDefaults.standard.removeAll()
+//                         exit(0)
+//                         }) {
+//                             Text(str.reset.rawValue)
+//                         Image(systemName: "shield")
+//                         }
+//
+//                         Button(action: {
+//                         let LastTimeDay = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
+//                         UserDefaults.standard.set(LastTimeDay, forKey: Keys._LastTimeDay.rawValue)
+//                         }) {
+//                             Text(str.oneDayReset.rawValue)
+//                         Image(systemName: "shield")
+//                         }
+//
+//                        /*
+//                         @IBAction func share() {
+//                         //share(上の段)から遷移した際にシェアするアイテム
+//                         let activityItems: [Any] = [shareText, shareUrl]
+//                         let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: [LINEActivity(message: shareText), TwitterActivity(message: shareText)])
+//                         self.present(activityViewController, animated: true, completion: nil)
+//                         }
+//                         */
+//                    }label: {
+//                        Image(systemName: "line.horizontal.3")
+//                            .resizable()
+//                            .frame(width: 25, height: 15, alignment: .topLeading)
+//                    }
+//                    .padding(EdgeInsets(top: 20, leading: 0, bottom: 8, trailing: 10))
                 }
                 ToolbarItem(placement: .navigationBarLeading){
                     HStack{
@@ -237,29 +238,26 @@ struct StatusView: View {
 }
 struct StatusView2: View {
     var body: some View {
+        
+        
+        
         let bounds = UIScreen.main.bounds
         let w = bounds.width
         let h = bounds.height
-        let xt = 1
-        let xp = 0
-        let yt = 1
-        let yp = 0
-        let ps = 20
-        let pr = ps / 2
+        let ps: CGFloat = 20
         
-        
-        let leftShoulder=CGPoint(x: w*0.6, y: h*0.25)
-        let rightShoulder=CGPoint(x: w*0.4, y: h*0.25)
-        let leftElbow=CGPoint(x: w*0.63, y: h*0.35)
-        let rightElbow=CGPoint(x: w*0.38, y: h*0.35)
-        let leftWrist=CGPoint(x: w*0.65, y: h*0.5)
-        let rightWrist=CGPoint(x: w*0.35, y: h*0.5)
-        let leftHip=CGPoint(x: w*0.58, y: h*0.5)
-        let rightHip=CGPoint(x: w*0.42, y: h*0.5)
+        let leftShoulder=CGPoint(x: w*0.63, y: h*0.25)
+        let rightShoulder=CGPoint(x: w*0.37, y: h*0.25)
+        let leftElbow=CGPoint(x: w*0.63, y: h*0.40)
+        let rightElbow=CGPoint(x: w*0.37, y: h*0.40)
+        let leftWrist=CGPoint(x: w*0.63, y: h*0.55)
+        let rightWrist=CGPoint(x: w*0.37, y: h*0.55)
+        let leftHip=CGPoint(x: w*0.6, y: h*0.5)
+        let rightHip=CGPoint(x: w*0.4, y: h*0.5)
         let leftKnee=CGPoint(x: w*0.6, y: h*0.65)
         let rightKnee=CGPoint(x: w*0.4, y: h*0.65)
-        let leftAnkle=CGPoint(x: w*0.6, y: h*0.75)
-        let rightAnkle=CGPoint(x: w*0.4, y: h*0.75)
+        let leftAnkle=CGPoint(x: w*0.6, y: h*0.8)
+        let rightAnkle=CGPoint(x: w*0.4, y: h*0.8)
         
         ZStack{
             Path { path in
@@ -291,20 +289,19 @@ struct StatusView2: View {
             
             
             Path { path in
-                path.addEllipse(in: CGRect(x:35*xt+xp-pr, y: 130*yt+yp-pr, width: ps, height: ps))
-                path.addEllipse(in: CGRect(x:40*xt+xp-pr, y: 90*yt+yp-pr, width: ps, height: ps))
-                path.addEllipse(in: CGRect(x:45*xt+xp-pr, y: 55*yt+yp-pr, width: ps, height: ps))
-                path.addEllipse(in: CGRect(x:85*xt+xp-pr, y: 55*yt+yp-pr, width: ps, height: ps))
-                path.addEllipse(in: CGRect(x:90*xt+xp-pr, y: 90*yt+yp-pr, width: ps, height: ps))
-                path.addEllipse(in: CGRect(x:95*xt+xp-pr, y: 130*yt+yp-pr, width: ps, height: ps))
+                path.addArc(center: rightWrist, radius: ps, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
+                path.addArc(center: rightElbow, radius: ps, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
+                path.addArc(center: rightShoulder, radius: ps, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
+                path.addArc(center: leftShoulder, radius: ps, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
+                path.addArc(center: leftElbow, radius: ps, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
+                path.addArc(center: leftWrist, radius: ps, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
                 
-                
-                path.addEllipse(in: CGRect(x:50*xt+xp-pr, y: 200*yt+yp-pr, width: ps, height: ps))
-                path.addEllipse(in: CGRect(x:50*xt+xp-pr, y: 160*yt+yp-pr, width: ps, height: ps))
-                path.addEllipse(in: CGRect(x:50*xt+xp-pr, y: 120*yt+yp-pr, width: ps, height: ps))
-                path.addEllipse(in: CGRect(x:80*xt+xp-pr, y: 120*yt+yp-pr, width: ps, height: ps))
-                path.addEllipse(in: CGRect(x:80*xt+xp-pr, y: 160*yt+yp-pr, width: ps, height: ps))
-                path.addEllipse(in: CGRect(x:80*xt+xp-pr, y: 200*yt+yp-pr, width: ps, height: ps))
+                path.addArc(center: rightAnkle, radius: ps, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
+                path.addArc(center: rightKnee, radius: ps, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
+                path.addArc(center: rightHip, radius: ps, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
+                path.addArc(center: leftHip, radius: ps, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
+                path.addArc(center: leftKnee, radius: ps, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
+                path.addArc(center: leftAnkle, radius: ps, startAngle: .degrees(0), endAngle: .degrees(360), clockwise: true)
             }
             .fill(Color.yellow)
             .frame(width: bounds.width, height: bounds.height)
---
+++ ShiranAppNew/ui/parts/toolbarItemView.swift
@@ -0,0 +1,105 @@
+//
+//  toolbarItemView.swift
+//  ShiranAppNew
+//
+//  Created by user on 2022/03/13.
+//
+
+import SwiftUI
+struct toolbarItemView: View {
+    @EnvironmentObject var appState: AppState
+    var body: some View {
+        Menu{
+            Button(action: {
+                appState.isSettingView = true
+            }) {
+                Text(str.set.rawValue)
+                Image(systemName: "gear")//"gearshape")
+            }
+            Button(action: {
+                appState.isExplainView = true
+            }) {
+                Text(str.detailApp.rawValue)
+                Image(systemName: "info.circle")//"gearshape")
+            }
+            /*
+             if self.appState.isinAccount {
+             Button(action: {
+             appState.logout()
+             }) {
+             Text("ログアウト")
+             Image(systemName: "person")
+             }
+             }else{
+             Button(action: {
+             appState.isLogin = true
+             }) {
+             Text("アカウント設定")
+             Image(systemName: "person")
+             }
+             }*/
+            
+            Button(action: {
+                appState.isPrivacyPolicy = true
+            }) {
+                Text(str.privacy_policy.rawValue)
+                Image(systemName: "shield")
+            }
+            Button(action: {
+                guard let writeReviewURL = URL(string: "https://twitter.com/G0DhdLNn4SftTjc")
+                else { fatalError("Expected a valid URL") }
+                UIApplication.shared.open(writeReviewURL)
+            }) {
+                Text(str.twitter.rawValue)
+                Image(systemName: "rectangle.and.pencil.and.ellipsis")
+            }
+            Button(action: {
+                appState.isOpinionView = true
+            }) {
+                Text(str.opinion.rawValue)
+                Image(systemName: "rectangle.and.pencil.and.ellipsis")
+            }
+            /*
+             Button(action: {
+             guard let writeReviewURL = URL(string: "https://apps.apple.com/us/app/%E3%81%97%E3%82%89%E3%82%93%E3%83%97%E3%83%AA/id1584268203")
+             else { fatalError("Expected a valid URL") }
+             UIApplication.shared.open(writeReviewURL)
+             }) {
+             Text("アプリを評価する")
+             Image(systemName: "rectangle.and.pencil.and.ellipsis")
+             }*/
+            
+            
+             Button(action: {
+             UserDefaults.standard.removeAll()
+             exit(0)
+             }) {
+                 Text(str.reset.rawValue)
+             Image(systemName: "shield")
+             }
+             
+             Button(action: {
+             let LastTimeDay = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
+             UserDefaults.standard.set(LastTimeDay, forKey: Keys._LastTimeDay.rawValue)
+             }) {
+                 Text(str.oneDayReset.rawValue)
+             Image(systemName: "shield")
+             }
+             
+            /*
+             @IBAction func share() {
+             //share(上の段)から遷移した際にシェアするアイテム
+             let activityItems: [Any] = [shareText, shareUrl]
+             let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: [LINEActivity(message: shareText), TwitterActivity(message: shareText)])
+             self.present(activityViewController, animated: true, completion: nil)
+             }
+             */
+        }label: {
+            Image(systemName: "line.horizontal.3")
+                .resizable()
+                .frame(width: 25, height: 15, alignment: .topLeading)
+        }
+        .padding(EdgeInsets(top: 20, leading: 0, bottom: 8, trailing: 10))
+    }
+    
+}
